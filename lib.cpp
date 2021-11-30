19a4ee06f18f19135c5f058aae204f6ff7510f96
KevinNicoli <91669026+KevinNicoli@users.noreply.github.com>
2021-11-30 17:48:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+
+int main() {
+    char x;
+    cin >> x;
+    int ris=verifica(x);
+    if(ris){
+        cout << converti(x) << endl;
+    }else {
+        cout << "errore"<< endl;
+    }
+    return 0;
+}
