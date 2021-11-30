3fa7a713a31fa0fc9f56b83d0e36aef984b593b2
KevinNicoli <91669026+KevinNicoli@users.noreply.github.com>
2021-11-30 17:51:43 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,18 +1,6 @@
-#include "lib.h"
+#ifndef ESERCITAZIONE6_LIB_H
+#define ESERCITAZIONE6_LIB_H
+bool verifica(char a);
+char converti(char a);
 
-bool verifica (char a) {
-    if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-char converti (char a) {
-    if (a>=65 && a<=90){
-        a= a+32;
-        return a;
-    }else if (a>=97 && a<=122) {
-        a= a-32;
-        return a;
-    }
-}
+#endif //ESERCITAZIONE6_LIB_H
